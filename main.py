bf3c31af07a20f4f749ff01d3cc972f5ed87a10d
Emanuele A. <161335121+NumberZeroo@users.noreply.github.com>
2026-03-30 15:56:00 +0200

    v1.1.1: Fix APK upload logic

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ Builder.load_file(os.path.join(BASE_DIR, 'ui', 'series_books.kv'))
 Builder.load_file(os.path.join(BASE_DIR, 'ui', 'reader.kv'))
 Builder.load_file(os.path.join(BASE_DIR, 'ui', 'downloads.kv'))
 
-VERSION = "1.0.5"
+VERSION = "1.1.1"
 
 
 class KomgaApp(App):
